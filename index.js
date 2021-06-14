677959caf813f50eea8eb393f0e7790bb0f18f04
Divyansh Kamboj <60178033+Divyansh-10@users.noreply.github.com>
2021-06-14 16:46:59 +0530

    Replacing if statements with string concatination for dice image selection

--- index.js
+++ index.js
@@ -4,70 +4,26 @@
 var ran1 = Math.random();
 ran1 *= 6;
 ran1 = Math.floor(ran1) + 1;
+var image1 = "images/dice"+ran1+".png";
+
+document.getElementById("player1").src = image1;
 
 var ran2 = Math.random();
 ran2 *= 6;
 ran2 = Math.floor(ran2) + 1;
 
-if(ran1 == 1)
-{
-  document.getElementById("player1").src = "images/dice1.png";
-}
-if (ran1 == 2)
-{
-   document.getElementById("player1").src = "images/dice2.png";
-}
-if (ran1 == 3)
-{
-  document.getElementById("player1").src = "images/dice3.png";
-}
-if (ran1 == 4)
-{
-   document.getElementById("player1").src = "images/dice4.png";
-}
-if (ran1 == 5)
-{
-document.getElementById("player1").src = "images/dice5.png";
-}
-if (ran1 == 6)
-{
-document.getElementById("player1").src = "images/dice6.png";
-}
+var image2 = "images/dice"+ran2+".png";
+document.getElementById("player2").src = image2;
 
-
-if(ran2 == 1)
-{
-  document.getElementById("player2").src = "images/dice1.png";
-}
-if (ran2 == 2)
-{
-   document.getElementById("player2").src = "images/dice2.png";
-}
-if (ran2 == 3)
-{
-  document.getElementById("player2").src = "images/dice3.png";
-}
-if (ran2 == 4)
-{
-   document.getElementById("player2").src = "images/dice4.png";
-}
-if (ran2 == 5)
-{
-document.getElementById("player2").src = "images/dice5.png";
-}
-if (ran2 == 6)
-{
-document.getElementById("player2").src = "images/dice6.png";
-}
 
 if(ran1 > ran2)
 {
-  document.querySelector("h1").innerText = "Player1 Wins";
+  document.querySelector("h1").innerHTML = "<img class = 'p1 flag' src='https://image.flaticon.com/icons/png/512/2164/2164733.png' alt='flag-image'>Player1 Wins";
 }
 
 if(ran1 < ran2)
 {
-  document.querySelector("h1").innerText = "Player2 Wins";
+  document.querySelector("h1").innerHTML = "Player2 Wins<img class = 'p2 flag' src='https://image.flaticon.com/icons/png/512/2164/2164733.png' alt='flag-image'>";
 }
 
 if(ran1 == ran2)
